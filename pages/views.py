be1cdb4f67e178be826e85f5381b1c8e62c7236f
mtuchi <mtuchidev@gmail.com>
2021-11-11 13:14:28 +0300

    feat: format codes

--- pages/views.py
+++ pages/views.py
@@ -2,25 +2,32 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+    template_name = "pages/home.html"
+
 
 class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
+    template_name = "pages/about.html"
+
 
 class SpeakPageView(TemplateView):
-    template_name = 'pages/speak.html'
+    template_name = "pages/speak.html"
+
 
 class SponsorPageView(TemplateView):
-    template_name = 'pages/sponsor.html'
+    template_name = "pages/sponsor.html"
+
 
 class CovidPageView(TemplateView):
-    template_name = 'pages/covid.html'
+    template_name = "pages/covid.html"
+
 
 class CoCPageView(TemplateView):
-    template_name = 'pages/coc.html'
+    template_name = "pages/coc.html"
+
 
 class SchedulePageView(TemplateView):
-    template_name = 'pages/schedule.html'
+    template_name = "pages/schedule.html"
+
 
 class TicketPageView(TemplateView):
-    template_name = 'pages/ticket.html'
+    template_name = "pages/ticket.html"
