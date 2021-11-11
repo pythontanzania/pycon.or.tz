9a990fb29027cd725affa435c209da773ea7aa78
mtuchi <mtuchidev@gmail.com>
2021-11-11 04:07:26 +0300

    feat: design landing page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, SpeakPageView, SponsorPageView, CovidPageView, CoCPageView, SchedulePageView, TicketPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('speak/', SpeakPageView.as_view(), name='speak'),
+    path('sponsor/', SponsorPageView.as_view(), name='sponsor'),
+    path('covid-19/', CovidPageView.as_view(), name='covid'),
+    path('coc/', CoCPageView.as_view(), name='coc'),
+    path('schedule/', SchedulePageView.as_view(), name='schedule'),
+    path('ticket/', TicketPageView.as_view(), name='ticket'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,23 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
-
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+class SpeakPageView(TemplateView):
+    template_name = 'pages/speak.html'
+
+class SponsorPageView(TemplateView):
+    template_name = 'pages/sponsor.html'
+
+class CovidPageView(TemplateView):
+    template_name = 'pages/covid.html'
+
+class CoCPageView(TemplateView):
+    template_name = 'pages/coc.html'
+
+class SchedulePageView(TemplateView):
+    template_name = 'pages/schedule.html'
+
+class TicketPageView(TemplateView):
+    template_name = 'pages/ticket.html'
