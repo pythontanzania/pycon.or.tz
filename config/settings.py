f9277a6d0b3f242bee5eb8aeb96c0c1b0b907ae0
mtuchi <mtuchidev@gmail.com>
2021-11-11 18:59:23 +0300

    feat: add pycon.co.tz to allowed_host

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = '43)%4yx)aa@a=+_c(fn&kf3g29xax+=+a&key9i=!98zyim=8j'
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","pycon.co.tz"]
 
 # APPS
 # ------------------------------------------------------------------------------
