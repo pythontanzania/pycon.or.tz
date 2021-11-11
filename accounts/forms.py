4c9f7ea0a384208b5c6a264c5a5a09400eeed32f
mtuchi <mtuchidev@gmail.com>
2021-11-11 18:23:36 +0300

    fix: code indentation

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,14 +2,20 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
 
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('email', 'username',)
+        fields = (
+            "email",
+            "username",
+        )
+
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ('email', 'username',)
+        fields = (
+            "email",
+            "username",
+        )
