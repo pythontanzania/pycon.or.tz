afbe7b81b45b74dfb722f62e9ca9f8370918c9ba
mtuchi <mtuchidev@gmail.com>
2022-08-05 23:54:10 +0300

    feat: add pycon 2021 report
    closes #18

--- pycon2021/views.py
+++ pycon2021/views.py
@@ -1,33 +1,28 @@
 from django.views.generic import TemplateView
 
-
 class HomePageView(TemplateView):
     template_name = "pycon2021/home.html"
-
 
 class AboutPageView(TemplateView):
     template_name = "pycon2021/about.html"
 
-
 class SpeakPageView(TemplateView):
     template_name = "pycon2021/speak.html"
-
 
 class SponsorPageView(TemplateView):
     template_name = "pycon2021/sponsor.html"
 
+class ReportPageView(TemplateView):
+    template_name = "pycon2021/report.html"
 
 class CovidPageView(TemplateView):
     template_name = "pycon2021/covid.html"
 
-
 class CoCPageView(TemplateView):
     template_name = "pycon2021/coc.html"
 
-
 class SchedulePageView(TemplateView):
     template_name = "pycon2021/schedule.html"
-
 
 class TicketPageView(TemplateView):
     template_name = "pycon2021/ticket.html"
