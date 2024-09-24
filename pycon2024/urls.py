a9ae3038ae6d18650732cc64b8894dcbaba6f0d5
lupyana <lupyanambembati@gmail.com>
2024-09-24 12:35:16 +0300

    chore: update website

--- pycon2024/urls.py
+++ pycon2024/urls.py
@@ -4,10 +4,12 @@ from .views import (
     HomePageView,
     CoCPageView,
     SponsorPageView,
+    SchedulePageView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("coc/", CoCPageView.as_view(), name="coc"),
     path("sponsor/", SponsorPageView.as_view(), name="sponsor"),
+    path("schedule/", SchedulePageView.as_view(), name="schedule"),
 ]
