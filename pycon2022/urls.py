79481cec7b54e8483e0603d847df36a55fd48675
lupyana <lupyanambembati@gmail.com>
2024-09-30 16:12:20 +0300

    website updates

--- pycon2022/urls.py
+++ pycon2022/urls.py
@@ -13,9 +13,9 @@ from .views import (
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
+    path("about/", AboutPageView.as_view(), name="about2022"),
     path("speak/", SpeakPageView.as_view(), name="speak"),
-    path("sponsor/", SponsorPageView.as_view(), name="sponsor"),
+    path("sponsor/", SponsorPageView.as_view(), name="sponsor2022"),
     path("travel-guide/", TravelPageView.as_view(), name="travel"),
     path("coc/", CoCPageView.as_view(), name="coc2022"),
     path("schedule/", SchedulePageView.as_view(), name="schedule"),
