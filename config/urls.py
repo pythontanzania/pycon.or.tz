c35d5cddfc997218c22bba5ecb5be7dfb64b1925
lupyana <lupyanambembati@gmail.com>
2024-11-11 19:18:16 +0300

    feat: dynamicaly fetch sponsors

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 urlpatterns = [
     path("pyconadmin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
@@ -11,7 +11,7 @@ urlpatterns = [
     path("2021/", include("pycon2021.urls")),
     path("2022/", include("pycon2022.urls")),
     path("", include("pycon.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     import debug_toolbar
--- pycon/views.py
+++ pycon/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .models import Sponsor, SponsorCategory
 # Create your views here.
 def HomePageView(request):
-    return render(request, "pycon/home.html")
+    sponsors_by_category = {}
+    for category in SponsorCategory.choices:
+        category_name = category[0]
+        sponsors = Sponsor.objects.filter(category=category_name)
+        sponsors_by_category[category_name] = sponsors
+
+    context = {
+        'sponsors_by_category': sponsors_by_category
+    }
+    return render(request, "pycon/home.html", context)
 
 def SpeakPageView(request):
     return render(request, "pycon/speak.html")
@@ -11,4 +20,13 @@ def CocPageview(request):
     return render(request, "pycon/coc.html") 
 
 def SponsorPageView(request):
-    return render(request, "pycon/sponsor.html")
+    sponsors_by_category = {}
+    for category in SponsorCategory.choices:
+        category_name = category[0]
+        sponsors = Sponsor.objects.filter(category=category_name)
+        sponsors_by_category[category_name] = sponsors
+
+    context = {
+        'sponsors_by_category': sponsors_by_category
+    }
+    return render(request, "pycon/sponsor.html", context)
